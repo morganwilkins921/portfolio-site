4355a3ac53f6f8dbb4acb30d0687eafb960b5375
morganwilkins <morganwilkins921@gmail.com>
2025-01-26 12:31:20 -0500

    add interactivity to navigation menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log("Hi! Welcome to my Portfolio Site.");
+console.log ("Hi! Welcome to my Portfolio Site.");
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+      } else {
+        x.className = 'navtoggle';
+      }
+}
